956493b0212ba42f9a7176500b85b8b022d6b15d
VVVVVictor <wangmiaofei08@gmail.com>
2014-02-28 13:00:21 +0800

    module

---
+++ Crawler-renrendai/Crawler_renrendai.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import urllib, urllib2, cookielib
+import sys, string, time, os, re
+import csv
+from bs4 import BeautifulSoup
+import socket
+
+from tools_renrendai import *
+
+#----------------------------
+#main
+if login():
+	print('success')
+else:
+    print('failed!')
+print('oh?')
