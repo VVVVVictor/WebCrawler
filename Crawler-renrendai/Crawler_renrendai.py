1626249949ff0741ec667e6afd91089f1f72b158
VVVVVictor <wangmiaofei08@gmail.com>
2014-02-28 19:34:33 +0800

    404

--- Crawler-renrendai/Crawler_renrendai.py
+++ Crawler-renrendai/Crawler_renrendai.py
@@ -6,13 +6,63 @@ import sys, string, time, os, re
 import csv
 from bs4 import BeautifulSoup
 import socket
-
 from tools_renrendai import *
 
+#constant
+LOST_PAGE_LIMIT = int(10)
+
+#for crawl
+urlLoan = u'http://www.renrendai.com/lend/detailPage.action?loanId='
+#filedirectory = u'D:\datas\pythondatas\renrendai\\'
+headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.117 Safari/537.36', 'Host':'www.renrendai.com'}
+
+def getData(begin_page, end_page, filedirectory):
+    
+    starttime = time.clock()
+    lostPageCount = 0 #记录连续404的页面个数
+    lastpage = begin_page #记录抓取的最后一个有效页面
+    
+    for i in range(begin_page, end_page+1):
+        req = urllib2.Request(urlLoan+str(i), headers = headers)
+        try:
+            response = urllib2.urlopen(req)
+            m = response.read()
+            #print(m)
+            lastpage = i
+            response.close()
+        except (urllib2.URLError) as e:
+            if hasattr(e, 'code'):
+                print(str(e.code)+': '+str(e.reason))
+            else:
+                print(e.reason)
+            i = lastpage
+            continue
+        except socket.error as e:
+            print('ERROR] Socket error: '+str(e.errno))
+            i = lastpage 
+            continue
+        #end try&except
+        
+        print('Downloading '+str(i)+' web page...')
+        if analyzeData(m):
+            lostPageCount = 0
+        else:
+            print('404')
+            lostPageCount += 1
+            if(lostPageCount > LOST_PAGE_LIMIT):
+                print('You have got the latest page!')
+                break
+    #end for
+    
+    endtime = time.clock()
+    print(u'[execute time]:'+str(endtime-starttime)+'s')
+    return
+#end def getData()
+        
+    
 #----------------------------
 #main
+filedirectory = getConfig()
 if login():
-	print('success')
-else:
-    print('failed!')
-print('oh?')
+    print('Login success!')
+    getData(0, 2, filedirectory)
--- Crawler-renrendai/tools_renrendai.py
+++ Crawler-renrendai/tools_renrendai.py
@@ -7,33 +7,96 @@ import csv
 from bs4 import BeautifulSoup
 import socket
 
+configfileName = 'config'
+filedirectory = u'D:\\datas\\pythondatas\\renrendai\\'
+
 #For login
 urlLogin = u'https://www.renrendai.com/j_spring_security_check'
-urlIndex = u'http://www.renrendai.com'
+urlIndex = u'http://www.renrendai.com/'
 username = u'15120000823'
 password = u'wmf123456'
-headers={'Referer':'https://www.renrendai.com/loginPage.action','User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.117 Safari/537.36', 'Host':'www.renrendai.com'}
+headers={'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.117 Safari/537.36', 'Host':'www.renrendai.com'}
 
-#for crawl
-urlLoan = u'http://www.renrendai.com/lend/detailPage.action?loanId='
-
+#--------------------------------------------------
+#读取配置文件
+def getConfig():
+    global filedirectory, username, password
+    try:
+        configfile = open(os.getcwd()+'\\'+configfileName, 'r')
+        #line = configfile.readline()
+        pattern = re.compile(u'\s*(\w+)\s*=\s*(\S+)\s*')
+        for line in configfile:
+            #print line
+            m = pattern.match(line)
+            if m:
+                if m.group(1) == u'filedirectory':
+                    filedirectory =  m.group(2)+'\\'
+                    '''
+                    tempchar = filedirectory[len(filedirectory)-1]
+                    if tempchar != u'\\' and tempchar != u'/':
+                        print('temp')
+                        filedirectory = filedirectory + '\\'
+                        '''
+                elif m.group(1) == u'username':
+                    username = m.group(2)
+                elif m.group(1) == u'password':
+                    password = m.group(2)
+                #print filedirectory
+        configfile.close()
+    except:
+        configfile = open(os.getcwd()+'\\'+configfileName, 'wb')
+        configfile.write('filedirectory = '+filedirectory+'\n')
+        configfile.write('username = '+username+'\n')
+        configfile.write('password = '+password+'\n')
+        configfile.close()
+        print('Create new config file!')
+    
+    print('[CONFIG]')
+    print('filedirectory = '+filedirectory)
+    print('username = '+username)
+    print('password = '+password)
+    return filedirectory
+#end def getConfig()
+    
+#--------------------------------------------------
+#登录函数
 def login():
-	cj = cookielib.CookieJar()
-	opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-	urllib2.install_opener(opener)
-	
-	data = {'j_username':username, 'j_password':password, 'rememberme':'on', 'targetUrl':'http://www.renrendai.com', 'returnUrl':''}
-	postdata = urllib.urlencode(data)
-	
-	try:
-		req = urllib2.Request(urlLogin, postdata, headers)
-		result = urllib2.urlopen(req)
-		result.close()
-		
-        #comment
-	except:
-		print(u'[FAIL]Login failed. Please try again!')
-		return False
-	
-	return True
+    cj = cookielib.CookieJar()
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+    urllib2.install_opener(opener)
+
+    data = {'j_username':username, 'j_password':password, 'rememberme':'on', 'targetUrl':'http://www.renrendai.com', 'returnUrl':''}
+    postdata = urllib.urlencode(data)
+
+    try:
+        req = urllib2.Request(urlLogin, postdata, headers)
+        result = urllib2.urlopen(req)
+        if urlIndex != result.geturl(): #通过返回url判断是否登录成功
+            print(u'[FAIL]Wrong USERNAME or PASSWORD. Please try again!')
+            return False
+        result.close()
+    except:
+        print(u'[FAIL]Login failed. Please try again!')
+        return False
+    return True
 #end def login()
+
+#--------------------------------------------------
+#查看文件夹是否存在：若不存在，则创建
+def createFolder(filedirectory):
+    if os.path.isdir(filedirectory):
+        pass
+    else:
+        os.makedirs(filedirectory) #可以创建多级目录
+    return
+
+#--------------------------------------------------
+def analyzeData(webcontent):
+    soup = BeautifulSoup(webcontent)
+    
+    if soup.find('img', {'alt':'404'}):
+        return False #页面404
+        
+        
+    return True
+    
